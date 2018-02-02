21e3974973c062f136903dc11ff7470d49c27847
Nick Pittak <noobalert@gmail.com>
2018-02-02 15:53:27 -0800

    added a test aren, 3 baddies and 2 chars.  Chars and baddies are 96x96 the arena is 48x48 tiles

---
+++ assets/Outside.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Outside" tilewidth="48" tileheight="48" tilecount="128" columns="8">
+ <image source="../Art/img/tilesets/Outside_A5.png" width="384" height="768"/>
+</tileset>
